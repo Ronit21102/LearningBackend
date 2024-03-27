7b07a816fed7d8597f227719599062aca7bdc5ac
Ronit Roushan <ronit21102@gmail.com>
2024-03-27 11:37:54 +0530

    basics done

---
+++ debuggingAndImprovement/routes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const requestHandler = (req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    
+    if (url === "/") {
+      res.write("<html>");
+      res.write("<head><title>message me</title></head>");
+      res.write(
+        '<Body><form action="/message" method="POST"><input type="text" name="message"><Button type="submit">Submit</Button></Body>'
+      );
+      res.write("</html>");
+      return res.end();
+    }
+    if (url === "/message" && method == "POST") {
+      console.log("times");
+      const body = [];
+      req.on("data", (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+      });
+    
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody);
+        const message = parsedBody.split("=")[1];
+    
+        fs.writeFileSync("message.txt", message);
+      });
+    
+      res.statusCode = 302;
+      res.setHeader = ("Location", "/");
+      return res.end();
+    }
+}
+
+module.exports = requestHandler;
